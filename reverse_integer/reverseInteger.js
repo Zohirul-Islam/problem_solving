5cf1fbaab5a29805411bdc16258e506d7ec33a71
bijoy <bijoycse24@gmail.com>
2025-09-15 10:30:01 +0600

    reverse number

---
+++ reverse_integer/reverseInteger.js
@@ -0,0 +1,8 @@
+/* Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0. */
+const reverseInterger = (num) => {
+  let reversed =
+    parseInt(Math.abs(num).toString().split("").reverse().join("")) *
+    Math.sign(num);
+  return reversed;
+};
+console.log(reverseInterger(-125));
