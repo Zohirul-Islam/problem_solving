bc08cf0a9566ebe6c333b857293169065ddd7b0c
bijoy <bijoycse24@gmail.com>
2025-10-26 19:19:58 +0600

    remove duplicated from sorted array

---
+++ removeduplicatefromsortedarray/app.js
@@ -0,0 +1,15 @@
+function removeDuplicates(nums) {
+  if (nums.length === 0) return 0;
+
+  let i = 0;
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
+      i++;
+      nums[i] = nums[j];
+    }
+  }
+
+  return i + 1; 
+}
+
+console.log(removeDuplicates([2,2,5,1,6,2,0]))
